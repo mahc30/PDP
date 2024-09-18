c1ff3ca57d1606ace9ab60f157ae47a55f547c5c
mahc <dahinkpie@gmail.com>
2024-09-17 21:49:15 -0500

    Imperativo

--- app/src/main/java/upb/pdp/Main.java
+++ app/src/main/java/upb/pdp/Main.java
@@ -1,7 +1,34 @@
 package upb.pdp;
 
+import upb.pdp.models.Persona;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String[] nombres = new String[3];
+        String[] telefonos = new String[3];
+        String[] documentos = new String[3];
+        int[] edades = new int[3];
+
+        nombres[0] = "Carlos";
+        nombres[1] = "Alirio";
+        nombres[2] = "Juana";
+
+        telefonos[0] = "12345";
+        telefonos[1] = "54321";
+        telefonos[2] = "1251251";
+
+        documentos[0] = "13642513";
+        documentos[1] = "353515";
+        documentos[2] = "2623626";
+
+        edades[0] = 11;
+        edades[1] = 54;
+        edades[2] = 23;
+
+
+        for(int i = 0; i < 3; i++){
+            System.out.println("Nombre: " + nombres[i] + " telefono: " + telefonos[i] + " documento: " + documentos[i] + " Edad " + edades[i]);
+        }
     }
 }
